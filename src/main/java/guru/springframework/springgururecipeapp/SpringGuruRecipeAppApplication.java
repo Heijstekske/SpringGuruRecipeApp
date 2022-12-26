e597c5498c1ce62ac05a71b78cacbaff26b98c29
Heijstekske <heijstekker@gmail.com>
2022-12-26 10:36:20 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/springgururecipeapp/SpringGuruRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springgururecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruRecipeAppApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringGuruRecipeAppApplication.class, args);
+   }
+
+}
---
+++ src/test/java/guru/springframework/springgururecipeapp/SpringGuruRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springgururecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuruRecipeAppApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}

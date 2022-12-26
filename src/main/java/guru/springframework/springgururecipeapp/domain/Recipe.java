0ff88b09b17c7a6b76361b8d041c333db7d2d9f5
Heijstekske <heijstekker@gmail.com>
2022-12-26 11:35:41 +0100

    domain ingredients added with OneToMany annotation

--- src/main/java/guru/springframework/springgururecipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/springgururecipeapp/domain/Recipe.java
@@ -2,6 +2,8 @@ package guru.springframework.springgururecipeapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Recipe {
 
@@ -18,6 +20,9 @@ public class Recipe {
    private String directions;
    //todo add
    //private Difficulty difficulty;
+
+   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+   private Set<Ingredient> ingredients;
 
    @Lob
    private Byte[] image;
@@ -87,6 +92,14 @@ public class Recipe {
 
    public void setDirections(String directions) {
       this.directions = directions;
+   }
+
+   public Set<Ingredient> getIngredients() {
+      return ingredients;
+   }
+
+   public void setIngredients(Set<Ingredient> ingredients) {
+      this.ingredients = ingredients;
    }
 
    public Byte[] getImage() {

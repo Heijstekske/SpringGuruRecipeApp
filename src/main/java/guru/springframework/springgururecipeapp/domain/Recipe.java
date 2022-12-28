a15c2874f82e00b9174c41ab0976aeef9c1781f7
Heijstekske <heijstekker@gmail.com>
2022-12-28 09:25:03 +0100

    refactor bootstrap class

--- src/main/java/guru/springframework/springgururecipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/springgururecipeapp/domain/Recipe.java
@@ -116,6 +116,12 @@ public class Recipe {
       return ingredients;
    }
 
+   public Recipe addIngredient(Ingredient ingredient){
+      ingredient.setRecipe(this);
+      this.ingredients.add(ingredient);
+      return this;
+   }
+
    public void setIngredients(Set<Ingredient> ingredients) {
       this.ingredients = ingredients;
    }
@@ -134,6 +140,7 @@ public class Recipe {
 
    public void setNotes(Notes notes) {
       this.notes = notes;
+      notes.setRecipe(this);
    }
 
    public Set<Category> getCategories() {

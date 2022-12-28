7cb87fcafee55095ffb6e560cc5522e1a771a378
Heijstekske <heijstekker@gmail.com>
2022-12-28 09:58:23 +0100

    added project Lombok

--- src/main/java/guru/springframework/springgururecipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/springgururecipeapp/controllers/IndexController.java
@@ -1,16 +1,12 @@
 package guru.springframework.springgururecipeapp.controllers;
 
-import guru.springframework.springgururecipeapp.domain.Category;
-import guru.springframework.springgururecipeapp.domain.UnitOfMeasure;
-import guru.springframework.springgururecipeapp.repositories.CategoryRepository;
-import guru.springframework.springgururecipeapp.repositories.UnitOfMeasureRepository;
 import guru.springframework.springgururecipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +18,7 @@ public class IndexController {
 
    @RequestMapping({"", "/", "/index"})
    public String getIndexPage(Model model) {
+      log.debug("Getting Index Page");
 
       model.addAttribute("recipes", recipeService.getRecipes());
 
